39896c215124894ac338c155ea994438133cd166
Denis Chueshkov <kot_denis@mail.ru>
2020-05-05 19:18:41 +0300

    feat: complete combinator task

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,29 @@ package subtask2
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var itog = 0
+        var i = 0
+        while (i < array[1] + 1) {
+            if (array[0] == (fact(array[1])/(fact(array[1]-i) * fact(i))).toInt()) {
+                itog = i
+                i = array[1] + 2
+            }
+            i++
+        }
+        if (itog > 0)
+            return itog
+        else
+            return null
+    }
+    fun fact (numb: Int): Long {
+        var It : Long
+        It = 1
+        var i = 1
+        while (i < numb + 1) {
+            It *= i
+            i++
+        }
+        return It
     }
 }
