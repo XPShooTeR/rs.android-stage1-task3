094f07d6d755ca3522ffda5408638ff550d4910d
Denis Chueshkov <kot_denis@mail.ru>
2020-05-05 15:35:26 +0300

    feat: complete polynomialconverter task

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -2,8 +2,45 @@ package subtask1
 
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        var itogStr = ""
+        var i = 0
+        val n = numbers.size
+        var timeIntValue = ""
+        while (i<n){
+            if (i != 0) {
+                if (numbers[i] != 0)
+                    itogStr = itogStr + " "
+                if (numbers[i] > 0) {
+                    itogStr = itogStr + "+ "
+                    timeIntValue = numbers[i].toString()
+                }
+                if (numbers[i] < 0) {
+                    itogStr = itogStr + "- "
+                    timeIntValue = (numbers[i] * (-1)).toString()
+                }
+                if (numbers[i] != 0) {
+                    if (((numbers[i] == 1) or (numbers[i] == -1)) and (i != (n-1)))
+                        timeIntValue = ""
+                    when (i) {
+                        n - 1 -> itogStr = itogStr + timeIntValue
+                        n - 2 -> itogStr = itogStr + timeIntValue + "x"
+                        else -> itogStr = itogStr + timeIntValue + "x^" + (n - 1 - i).toString()
+                    }
+                }
+            }
+            else
+                if (i == 0)
+                    when (numbers[0]) {
+                        1 -> itogStr = itogStr + "x^" + (n - 1).toString()
+                        -1 -> itogStr = itogStr + "-x^" + (n - 1).toString()
+                        else -> itogStr = itogStr + numbers[0].toString() + "x^" + (n - 1).toString()
+                    }
+            i++
+        }
+        if (n>0)
+            return itogStr
+        else
+            return null
     }
 }
