438cc8342b6e64963e1af712be4572840c9c8441
Denis Chueshkov <kot_denis@mail.ru>
2020-05-06 22:09:08 +0300

    feat: edit fullbinary task

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -1,9 +1,53 @@
 package subtask6
 
+import kotlin.math.*
+
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+
+        val strItog = ""
+        if (count % 2 == 0)
+            return "[]"
+        else {
+            var i = ((count + 1) / 2)
+            var j : Int
+            var itogStr = ""
+            if (count == 1)
+                return "[[0]]"
+            if (count == 3)
+                return "[[0,0,0]]"
+            itogStr += "[[0,0,0"
+            while (i > 2) {
+
+                j = 1
+                while (j < 2.toDouble().pow(i-1).toInt())
+                {
+                    j++
+                }
+                i--
+            }
+            return strItog
+        }
+    }
+    fun inttobin(inbyn : Int): String {
+        var ost = inbyn
+        var itogstr = ""
+        var itogstrrev = ""
+        var i : Int
+        while (ost > 1) {
+            itogstr += (ost%2).toString()
+            if (ost%2 == 1)
+                ost = (ost - 1)/2
+            else
+                ost = ost/2
+        }
+        itogstr += ost.toString()
+        i = itogstr.length
+        while (i > 0){
+            itogstrrev += itogstr[i-1]
+            i--
+        }
+        return itogstrrev
     }
 }
